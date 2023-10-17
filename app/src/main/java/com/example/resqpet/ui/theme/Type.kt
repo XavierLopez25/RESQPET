cd44a738d37eeadf222f859c5421b7e7a95eea2a
XavierLopez25 <lop22716@uvg.edu.gt>
2023-10-17 10:11:49 -0600

    First commit. Added settings for a new DefaultCustomFont for all app, added GoogleFonts implementation and certificates.

---
+++ app/src/main/java/com/example/resqpet/ui/theme/Type.kt
@@ -0,0 +1,73 @@
+package com.example.resqpet.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.unit.sp
+import com.example.resqpet.R
+
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Montserrat")
+
+val Montserrat = FontFamily(
+    Font(
+        googleFont = fontName,
+        fontProvider = provider
+    )
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+).defaultFontFamily(fontFamily = Montserrat)
+
+fun Typography.defaultFontFamily(fontFamily: FontFamily): Typography {
+    return this.copy(
+        displayLarge = this.displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = this.displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = this.displaySmall.copy(fontFamily = fontFamily),
+        headlineLarge = this.headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = this.headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = this.headlineSmall.copy(fontFamily = fontFamily),
+        titleLarge = this.titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = this.titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = this.titleSmall.copy(fontFamily = fontFamily),
+        bodyLarge = this.bodyLarge.copy(fontFamily = fontFamily),
+        bodyMedium = this.bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = this.bodySmall.copy(fontFamily = fontFamily),
+        labelLarge = this.labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = this.labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = this.labelSmall.copy(fontFamily = fontFamily)
+    )
+}
