8096a65f67e47acc305cb35643b722925e4982c0
XavierLopez25 <lop22716@uvg.edu.gt>
2023-10-21 19:00:23 -0600

    Added package distribution for screens 2 to 7, pending the adding of navigation.

--- app/src/main/java/com/example/resqpet/MainActivity.kt
+++ app/src/main/java/com/example/resqpet/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Donation()
+                   Greeting(name = "Android")
                 }
             }
         }
